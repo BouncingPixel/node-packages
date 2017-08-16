c11968235b46a65532d94284ebcd41b94c33cf0d
Matthew Hall <mhall@bouncingpixel.com>
2017-08-16 09:47:39 -0500

    Adding webpack dev error display

--- default-express/src/initializers/webpack-dev.js
+++ default-express/src/initializers/webpack-dev.js
@@ -1,5 +1,26 @@
 const winston = require('winston');
 const nconf = require('nconf');
+const AnsiToHtml = require('ansi-to-html');
+const ansiConverter = new AnsiToHtml({
+  colors: {
+    0: '#000',
+    1: '#A00',
+    2: '#0A0',
+    3: '#A90',
+    4: '#00A',
+    5: '#A0A',
+    6: '#0AA',
+    7: '#AAA',
+    8: '#999',
+    9: '#F99',
+    10: '#9F9',
+    11: '#FF9',
+    12: '#99F',
+    13: '#F9F',
+    14: '#9FF',
+    15: '#FFF'
+  }
+});
 
 const webpackConfigPath = nconf.get('webpackConfigPath');
 
@@ -28,10 +49,24 @@ function makeMiddleware(app, webpack, webpackDevMiddleware, config) {
   const webpackcompiler = webpack(config);
 
   const devMiddlewareInst = webpackDevMiddleware(webpackcompiler, {
-    publicPath: config.output.publicPath
+    publicPath: config.output.publicPath,
+    serverSideRender: true
   });
 
-  app.use(devMiddlewareInst);
+  app.use(function(req, res, next) {
+    devMiddlewareInst(req, res, function() {
+      const stats = res.locals.webpackStats.toJson('errors-only');
+
+      if (stats && stats.errors.length) {
+        const errors = stats.errors.map(e => `<pre>${ansiConverter.toHtml(e)}</pre>`);
+
+        res.send(`<html><head><title>Error</title><style>body {background-color:#000;color:#fff;font-size:14px;line-height:20px;}</style></head><body> ${errors.join('<hr><br>')} </body></html>`);
+        return;
+      }
+
+      next();
+    });
+  });
 }
 
 function doNothing() {
